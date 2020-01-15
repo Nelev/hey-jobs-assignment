ac3964b9ec37094c94b89fc5df21f63f14532a47
msmacchia <masm@dhigroup.com>
2020-01-15 10:33:25 +0100

    chore: move query hook to shatred hooks

--- src/views/JobDetails/index.tsx
+++ src/views/JobDetails/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom'
-
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 
 import { fetchJob } from "../../actions/jobs"
+import { useQuery } from "../../hooks/hooks"
 import { IJob } from '../../model/IJob';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -17,12 +16,8 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const JobDetails: React.FC = () => {
     const dispatch = useDispatch()
-    const classes = useStyles();
-
-    const useQuery = () => {
-        return new URLSearchParams(useLocation().search);
-    }
     const query = useQuery();
+    const classes = useStyles();
 
     const job: IJob | null = useSelector(
         (state: any) => state.jobs.job
